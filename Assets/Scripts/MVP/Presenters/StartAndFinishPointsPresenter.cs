cafa754ff32292e57c8724aa150283f90c1e7091
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2020-09-26 03:51:43 +0300

    wall clicking bug fixed

--- Assets/Scripts/MVP/Presenters/StartAndFinishPointsPresenter.cs
+++ Assets/Scripts/MVP/Presenters/StartAndFinishPointsPresenter.cs
@@ -6,9 +6,13 @@ public class StartAndFinishPointsPresenter : UserCommandHandlerPresenterBase
     private Transform _view;
     [SerializeField]
     private Vector2IntValue _coordinatesValue;
+    [SerializeField]
+    private Map _map;
 
     protected override void handleCommand(Vector2Int coords)
     {
+        if (!_map[coords.x, coords.y].IsTraversable)
+            return;
         _view.position = new Vector3(coords.x, 0, coords.y);
         _coordinatesValue.Value = coords;
     }
